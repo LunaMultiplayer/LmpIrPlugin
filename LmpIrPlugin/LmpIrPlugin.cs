068bbb0751faf6811b7bd3e0ea4f01b63d962909
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-10-03 17:57:59 +0200

    Initial commit

---
+++ LmpIrPlugin/LmpIrPlugin.cs
@@ -0,0 +1,84 @@
+﻿using InfernalRobotics.Module;
+using LmpClient.Events;
+using UnityEngine;
+
+namespace LmpIrPlugin
+{
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class LmpIrPlugin : MonoBehaviour
+    {
+        public static LmpIrPlugin Singleton { get; set; }
+
+        public void Awake()
+        {
+            Singleton = this;
+            DontDestroyOnLoad(this);
+
+            Debug.Log("[LmpIRPlugin] LMP-IR Plugin started!");
+
+            PartModuleEvent.onPartModuleBoolFieldProcessed.Add(PartModuleBoolFieldChanged);
+            PartModuleEvent.onPartModuleIntFieldProcessed.Add(PartModuleIntFieldChanged);
+            PartModuleEvent.onPartModuleFloatFieldProcessed.Add(PartModuleFloatFieldChanged);
+            PartModuleEvent.onPartModuleDoubleFieldProcessed.Add(PartModuleDoubleFieldChanged);
+            PartModuleEvent.onPartModuleVectorFieldProcessed.Add(PartModuleVectorFieldChanged);
+            PartModuleEvent.onPartModuleQuaternionFieldProcessed.Add(PartModuleQuaternionFieldChanged);
+            PartModuleEvent.onPartModuleStringFieldProcessed.Add(PartModuleStringFieldChanged);
+            PartModuleEvent.onPartModuleObjectFieldProcessed.Add(PartModuleObjectFieldChanged);
+            PartModuleEvent.onPartModuleEnumFieldProcessed.Add(PartModuleEnumFieldChanged);
+        }
+
+        private void PartModuleEnumFieldChanged(ProtoPartModuleSnapshot module, string field, int value, string data3)
+        {
+            UpdatePartPos(module, field, value);
+        }
+
+        private void PartModuleObjectFieldChanged(ProtoPartModuleSnapshot module, string field, object value)
+        {
+            UpdatePartPos(module, field, value);
+        }
+
+        private void PartModuleStringFieldChanged(ProtoPartModuleSnapshot module, string field, string value)
+        {
+            UpdatePartPos(module, field, value);
+        }
+
+        private void PartModuleQuaternionFieldChanged(ProtoPartModuleSnapshot module, string field, Quaternion value)
+        {
+            UpdatePartPos(module, field, value);
+        }
+
+        private void PartModuleVectorFieldChanged(ProtoPartModuleSnapshot module, string field, Vector3 value)
+        {
+            UpdatePartPos(module, field, value);
+        }
+
+        private void PartModuleDoubleFieldChanged(ProtoPartModuleSnapshot module, string field, double value)
+        {
+            UpdatePartPos(module, field, value);
+        }
+
+        private void PartModuleFloatFieldChanged(ProtoPartModuleSnapshot module, string field, float value)
+        {
+            UpdatePartPos(module, field, value);
+        }
+
+        private void PartModuleIntFieldChanged(ProtoPartModuleSnapshot module, string field, int value)
+        {
+            UpdatePartPos(module, field, value);
+        }
+
+        private void PartModuleBoolFieldChanged(ProtoPartModuleSnapshot module, string field, bool value)
+        {
+            UpdatePartPos(module, field, value);
+        }
+
+        private void UpdatePartPos(ProtoPartModuleSnapshot module, string fieldName, object value)
+        {
+            if (module.moduleRef != null && module.moduleRef is ModuleIRServo)
+            {
+                module.moduleRef.Fields[fieldName].SetValue(value, module.moduleRef);
+                module.moduleRef.OnStart(PartModule.StartState.None);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+<p align="center">
+    <a href="https://paypal.me/gavazquez"><img src="https://img.shields.io/badge/paypal-donate-yellow.svg?style=flat&logo=paypal" alt="PayPal"/></a>
+    <a href="https://discord.gg/wKVMhWQ"><img src="https://img.shields.io/discord/378456662392045571.svg?style=flat&logo=discord&label=discord" alt="Chat on discord"/></a>
+    <a href="../../releases"><img src="https://img.shields.io/github/release/lunamultiplayer/lmpirplugin.svg?style=flat&logo=github&logoColor=white" alt="Latest release" /></a>
+    <a href="../../releases"><img src="https://img.shields.io/github/downloads/lunamultiplayer/lmpirplugin/total.svg?style=flat&logo=github&logoColor=white" alt="Total downloads" /></a>
+</p>
+
+---
+
+## [Luna Multiplayer Mod (LMP)](../../../LunaMultiplayer) - [Infernal Robotics (IR)](../../../../MagicSmokeIndustries/InfernalRobotics) Plugin
+
+*Plugin to make IR work with LMP*
+
+### Status:
+
+|   Branch   |   Build  | Last commit  |   Activity    |    Commits    |
+| ---------- | -------- | ------------ | ------------- | ------------- |
+| **master** |[![AppVeyor](https://img.shields.io/appveyor/ci/gavazquez/lmpirplugin/master.svg?style=flat&logo=appveyor)](https://ci.appveyor.com/project/gavazquez/lmpirplugin/branch/master) | [![Last commit](https://img.shields.io/github/last-commit/lunamultiplayer/lmpirplugin/master.svg?style=flat&logo=github&logoColor=white)](../../commits/master) | [![Commit activity](https://img.shields.io/github/commit-activity/y/lunamultiplayer/lmpkisplugin.svg?style=flat&logo=github&logoColor=white)](../../commits/master) | [![Commits since release](https://img.shields.io/github/commits-since/lunamultiplayer/lmpkisplugin/latest.svg?style=flat&logo=github&logoColor=white)](../../commits/master)
+
+<p align="center">
+    <a href="https://ci.appveyor.com/project/gavazquez/lmpirplugin/history"><img src="https://buildstats.info/appveyor/chart/gavazquez/lmpirplugin?buildCount=100" alt="Build history"/></a>
+</p>
+
+---
+
+<p align="center">
+  <a href="mailto:gavazquez@gmail.com"><img src="https://img.shields.io/badge/email-gavazquez@gmail.com-blue.svg?style=flat" alt="Email: gavazquez@gmail.com" /></a>
+  <a href="./LICENSE"><img src="https://img.shields.io/github/license/lunamultiplayer/LunaMultiPlayer.svg" alt="License" /></a>
+</p>
